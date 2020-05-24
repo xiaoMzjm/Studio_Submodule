13457c95bf6403112193ca989ba220952fcc0553
zhangjiaming <383248689@qq.com>
2020-05-24 18:52:05 +0800

    日期工具

--- common/src/main/java/com/base/common/util/DateUtil.java
+++ common/src/main/java/com/base/common/util/DateUtil.java
@@ -222,7 +222,7 @@ public class DateUtil {
     public static Date getFirstDayOfMonth(Integer year, Integer month) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.MONTH, month - 1);
         calendar.set(Calendar.DAY_OF_MONTH, 1);
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
