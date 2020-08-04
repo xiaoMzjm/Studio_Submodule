5c5de092ff1250bf3748787650b55504c704213e
zhangjiaming <383248689@qq.com>
2020-08-04 23:33:39 +0800

    日期工具

--- common/src/main/java/com/base/common/util/DateUtil.java
+++ common/src/main/java/com/base/common/util/DateUtil.java
@@ -534,4 +534,34 @@ public class DateUtil {
         calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTime();
     }
+
+    public static int getDayNumOfMonth(int year, int month) {
+        int days = 0;
+        if (month != 2) {
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    days = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    days = 30;
+
+            }
+        } else {
+            // 闰年
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                days = 29;
+            else
+                days = 28;
+        }
+        return days;
+    }
 }
