10ff4c7ddf2ae1fe84de7233a61397f9dd3f2bdf
zhangjiaming <383248689@qq.com>
2020-08-05 23:40:39 +0800

    sqlutil

---
+++ common/src/main/java/com/base/common/util/SqlUtil.java
@@ -0,0 +1,42 @@
+package com.base.common.util;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+
+/**
+ * @author:小M
+ * @date:2020/8/5 11:40 PM
+ */
+public class SqlUtil {
+
+    private String inStrList(List<String> list){
+        if(CollectionUtils.isEmpty(list)) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String s : list) {
+            sb.append("'" + s + "'").append(",");
+        }
+        String str = sb.toString();
+        if(str.endsWith(",")) {
+            str = str.substring(0,str.length()-1);
+        }
+        return "(" + str + ")";
+    }
+
+    private String inIntList(List<Integer> list){
+        if(CollectionUtils.isEmpty(list)) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(Integer i : list) {
+            sb.append(i).append(",");
+        }
+        String str = sb.toString();
+        if(str.endsWith(",")) {
+            str = str.substring(0,str.length()-1);
+        }
+        return "(" + str + ")";
+    }
+}
